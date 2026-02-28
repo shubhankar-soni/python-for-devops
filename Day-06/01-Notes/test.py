23526c6e37266fe9411d2b302a78de699c90bf6c
shubhankar-soni <84657592+shubhankar-soni@users.noreply.github.com>
2026-02-28 17:11:07 +0530

    day-06 examples and practice

---
+++ Day-06/01-Notes/test.py
@@ -0,0 +1,61 @@
+# my_list = ["shubh", "28", "Hi1AA", 9, '&']
+
+# result1 = my_list is not my_list
+# print(result1)
+
+# result2 = '&' in my_list
+# print(result2)
+
+# a = 5
+# b = 6
+
+# result1 = a & b
+
+# result2 = a | b
+
+# print(result1, result2)
+
+# total = 10
+
+# # total += total
+# # print(total)
+# # total -= total
+# # print(total)
+# # total *= total
+# # print(total)
+# total //= total
+# print(total)
+# print (50 // 17)
+
+# a = 20
+# b = 5
+# x = True
+# y = False
+
+# result1 = x and y
+# result2 = x or y
+
+# print(result1, result2)
+
+
+
+# result = a < b 
+# result1 = a > b
+# result2 = a <=b
+# result4 = a >=b
+# result5 = a == b
+# result6 = a != b
+
+# print(result5, '\n', result6 )
+
+## arithmetic operators ##
+# sum = a+b
+# sub = a-b
+# product = a*b
+# division = a/b
+# print(sum, "ye h sum")
+# print(sub, "ye h subtraction")
+# print(product, "ye h multiplication")
+# print(division, "ye hai division, jo ki quotient return karega")
+##############################
+
---
+++ Day-08/test.py
@@ -0,0 +1,8 @@
+my_tuple = (1,2,4, "Shubh", "A")
+print(my_tuple)
+my_tuple = (7,8,9) + my_tuple
+print(my_tuple.index)
+
+
+
+
