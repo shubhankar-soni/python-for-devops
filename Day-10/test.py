60d65397d251eb326afb821d68e9da618207a33c
shubhankar-soni <84657592+shubhankar-soni@users.noreply.github.com>
2026-03-25 16:36:25 +0000

    test.py day 10 update

--- Day-10/test.py
+++ Day-10/test.py
@@ -4,5 +4,15 @@ folders = input("Provide list of folder names with spaces in between: ").split()
 print(folders)
 
 for folder in folders:
-    files = os.listdir(folder)    
-    print(files)
+    try:
+        files = os.listdir(folder)
+    except FileNotFoundError:
+        print("Please provide a valid folder name " +folder+ " is not a valid directory")
+        continue
+    except PermissionError:
+        print("No access to " +folder)
+        continue
+
+    print("Listing files for the folder====" +folder)
+    for file in files:
+        print(file)
